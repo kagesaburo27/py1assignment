084e1714c12af8975e8de62dba5ce69c932abf96
kagesaburo27 <70998876+kagesaburo27@users.noreply.github.com>
2021-09-27 00:52:57 +0600

    Update CoinGeckoAssignment.py

--- src/CoinGeckoAssignment.py
+++ src/CoinGeckoAssignment.py
@@ -1,16 +1,10 @@
 from pycoingecko import CoinGeckoAPI
-import json
 cg = CoinGeckoAPI()
 def getCoins(n):
-    coins = cg.get_coins_markets(vs_currency="usd", order="market_cap_desc", per_page=n, page=1, sparkline="falset")
-    return coins
-def getCoinsDesc(n):
-    coins = cg.get_coins_markets(vs_currency="usd", per_page=n, page=1, sparkline="falset")
-    result = sorted(coins, key=lambda coin: coin['current_price'], reverse = True)
-    return result  
+    coins = cg.get_coins_markets(vs_currency="usd", per_page=n)
+    result = sorted(coins, key = lambda coin: coin['current_price'], reverse = True)
+    for i in result:
+        print(i["id"], i["current_price"])
 print("Please, input your number")
 n = int(input())
-print("-------------------List using Coingecko sorting---------------------")
-print(json.dumps(getCoins(n), indent=2))
-print("-------------------List using python sorting------------------------")
-print(json.dumps(getCoinsDesc(n), indent=2))
+getCoins(n)
