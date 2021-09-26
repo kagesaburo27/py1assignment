0a102b9119cfd8b78f7c978a2608494c12b9a4d7
kagesaburo27 <70998876+kagesaburo27@users.noreply.github.com>
2021-09-27 01:09:22 +0600

    Update CoinGeckoAssignment.py

--- src/CoinGeckoAssignment.py
+++ src/CoinGeckoAssignment.py
@@ -1,10 +1,10 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 def getCoins(n):
-    coins = cg.get_coins_markets(vs_currency="usd", per_page=n)
+    coins = cg.get_coins_markets(vs_currency="usd")
     result = sorted(coins, key = lambda coin: coin['current_price'], reverse = True)
-    for i in result:
-        print(i["id"], i["current_price"])
+    for i in range(n):
+        print(result[i]["id"], result[i]["current_price"])
 print("Please, input your number")
 n = int(input())
 getCoins(n)
